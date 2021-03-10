1fe005f68c6b922b026974653e191d186db1eb7c
diogo <diogoantonio47@hotmail.com>
2021-03-10 19:37:14 -0300

    atualizações

--- App.js
+++ App.js
@@ -1,114 +1,71 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React , {Component} from 'react'
+import {View, Text, StyleSheet, FlatList, ScrollView} from 'react-native'
+import Footer from './src/components/Footer'
+import Header from './src/components/Header'
+import Pessoa from './src/components/Produto'
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      produto:[
+        { id: '1',nome: 'Arroz',valor: 20,marca: 'Vasconcelos',quantidade: 12,referencia: '001', desconto: '10%', promocao: 'Leve 5 pague 4'},
+        { id: '2',nome: 'Feijão',valor: 10,marca: 'Vasconcelos',quantidade: 15,referencia: '002', desconto: '5%', promocao: 'Leve 6 pague 5' },
+        { id: '3',nome: 'Detergente',valor: 1.99,marca: 'Ypê',quantidade: 100,referencia: '003', desconto: '20%', promocao: 'Leve 10 pague 9' },
+        { id: '4',nome: 'Macarrão',valor: 4.99,marca: 'Amália',quantidade: 50,referencia: '004', desconto: '5%', promocao: 'Leve 4 pague 3' },
+        { id: '5',nome: 'Bolacha',valor: 3.99,marca: 'Mabel',quantidade: 100,referencia: '005', desconto: '25%', promocao: 'Leve 2 pague 1' },
+        { id: '6',nome: 'Achocolatado',valor: 9.99,marca: 'Nescau',quantidade: 70,referencia: '006', desconto: '30%', promocao: 'Leve 3 pague 1' },
+      ]
+    }
+  }
+  render(){
+    return(
+      <>
+      <Header />
+    
+      <View style={styles.container}>
+       <Text style={styles.titulo}>Lista de Produtos</Text>
+        <View>
+          
+          <FlatList 
+            data={this.state.produto}
+            keyExtractor={(item)=> item.id}
+            renderItem={({item})=> 
+              <Pessoa 
+                nome={item.nome}  
+                valor={item.valor}  
+                marca={item.marca}  
+                quantidade={item.quantidade}  
+                desconto={item.desconto}
+                promocao={item.promocao}  
+                referencia={item.referencia}  
+                
+              />}
+              showsVerticalScrollIndicator={false}
+          />
+        </View>
+      </View>
+    
+      <Footer />
+      </>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+  titulo:{
+    fontSize: 30,
+    color: '#1DA1F2',
+    fontWeight: 'bold',
+    margin: 5
+  }
 
-export default App;
+})
+
+export default App
